bc933a965309b0b89f48d9611897123b920a289c
j0n4s <jonas.esser@gmail.com>
2023-10-03 00:11:33 +0200

    #10 Removed personal config
    Sorry. My secret was in there. Good luck -> already invalid

--- src/core/plugins/athena-plugin-discord-allow-list/server/config.ts
+++ src/core/plugins/athena-plugin-discord-allow-list/server/config.ts
@@ -1,15 +1,15 @@
 export const AllowListConfig = {
     // Discord Guild ID
-    guild: '', //'899586018183872573',
+    guild: '',
     // Discord Role ID
-    role: '', //'899587241985310721',
+    role: '',
     // Discord Bot Token Secret
-    token: '', //'ODk5NjAyMzE5OTQxOTcyMDE5.G4Gv-O.Dp5aahMiYb8vmAdz-WTSFwS1d7OFW_W2sqwb94',
+    token: '',
     // Change these responses, they are shown to a user on kick.
     responses: {
-        notOnList: 'Not on allow list. Visit https://my-gp.de/ and join discord server.',
-        noDiscordForAccount: 'No discord linked for account. Visit https://my-gp.de/ and join discord server.',
-        notInDiscordServer: 'You are not in the Discord Server. Visit https://my-gp.de/ and join discord server.',
-        notAllowListed: 'You are not allow listed. Visit https://my-gp.de/ and join discord server.',
+        notOnList: 'Not on allow list.',
+        noDiscordForAccount: 'No discord linked for account. Speak to an Admin.',
+        notInDiscordServer: 'You are not in the Discord Server.',
+        notAllowListed: 'You are not allow listed.',
     },
 };
