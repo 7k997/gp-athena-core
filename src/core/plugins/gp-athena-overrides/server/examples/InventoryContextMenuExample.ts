49715cb953109a75da39ce0822b58eee164c231f
j0n4s <jonas.esser@gmail.com>
2023-12-29 03:04:34 +0100

    #1 examples uncommented

--- src/core/plugins/gp-athena-overrides/server/examples/InventoryContextMenuExample.ts
+++ src/core/plugins/gp-athena-overrides/server/examples/InventoryContextMenuExample.ts
@@ -1,98 +1,98 @@
-import * as alt from 'alt-server';
-import * as Athena from '@AthenaServer/api/index.js';
-import { DefaultItemBehavior, Item, StoredItem } from '@AthenaShared/interfaces/item.js';
-import { ISharedItem } from '@AthenaPlugins/gp-items-shared/shared/interfaces.js';
-import { InventoryType } from '@AthenaPlugins/core-inventory/shared/interfaces.js';
+// import * as alt from 'alt-server';
+// import * as Athena from '@AthenaServer/api/index.js';
+// import { DefaultItemBehavior, Item, StoredItem } from '@AthenaShared/interfaces/item.js';
+// import { ISharedItem } from '@AthenaPlugins/gp-items-shared/shared/interfaces.js';
+// import { InventoryType } from '@AthenaPlugins/core-inventory/shared/interfaces.js';
 
-const enum ExampleActions {
-    Action1 = 'gp-athena-overrides:example:action1',
-}
-export class InventoryContextMenuExample {
-    static init() {
-        //TODO Example menu..check if it works. also check db, should not be saved! Too much data!
-        Athena.systems.inventory.factory.addInjection(
-            'fromStoredItem_AfterInjection',
-            InventoryContextMenuExample.addContextMenu,
-        );
+// const enum ExampleActions {
+//     Action1 = 'gp-athena-overrides:example:action1',
+// }
+// export class InventoryContextMenuExample {
+//     static init() {
+//         //TODO Example menu..check if it works. also check db, should not be saved! Too much data!
+//         Athena.systems.inventory.factory.addInjection(
+//             'fromStoredItem_AfterInjection',
+//             InventoryContextMenuExample.addContextMenu,
+//         );
 
-        alt.onClient(ExampleActions.Action1, InventoryContextMenuExample.action1);
-    }
+//         alt.onClient(ExampleActions.Action1, InventoryContextMenuExample.action1);
+//     }
 
-    static action1(player: alt.Player, type: InventoryType, slot: number) {
-        alt.log('action1');
-        //To something with item...
-    }
+//     static action1(player: alt.Player, type: InventoryType, slot: number) {
+//         alt.log('action1');
+//         //To something with item...
+//     }
 
-    static addContextMenu<CustomData = ISharedItem, CustomBehavior = DefaultItemBehavior>(
-        functionName: string,
-        item: Item<CustomBehavior & DefaultItemBehavior, CustomData>,
-    ) {
-        const contextMenuName = 'example context';
-        const clothingContextMenuName = 'example clothing context';
+//     static addContextMenu<CustomData = ISharedItem, CustomBehavior = DefaultItemBehavior>(
+//         functionName: string,
+//         item: Item<CustomBehavior & DefaultItemBehavior, CustomData>,
+//     ) {
+//         const contextMenuName = 'example context';
+//         const clothingContextMenuName = 'example clothing context';
 
-        //FIXME: Workaround - prevent double creation by removing old menue with same name
-        if (item.customEventsToCall) {
-            let index = item.customEventsToCall.findIndex((subMenu) => subMenu.name === contextMenuName);
-            if (index !== -1) {
-                item.customEventsToCall.splice(index, 1);
-            }
+//         //FIXME: Workaround - prevent double creation by removing old menue with same name
+//         if (item.customEventsToCall) {
+//             let index = item.customEventsToCall.findIndex((subMenu) => subMenu.name === contextMenuName);
+//             if (index !== -1) {
+//                 item.customEventsToCall.splice(index, 1);
+//             }
 
-            index = item.customEventsToCall.findIndex((subMenu) => subMenu.name === clothingContextMenuName);
-            if (index !== -1) {
-                item.customEventsToCall.splice(index, 1);
-            }
-        }
+//             index = item.customEventsToCall.findIndex((subMenu) => subMenu.name === clothingContextMenuName);
+//             if (index !== -1) {
+//                 item.customEventsToCall.splice(index, 1);
+//             }
+//         }
 
-        //FIXME: Workaround - prevent double creation by removing old menue with same name
-        if (item.customSubMenus) {
-            let index = item.customSubMenus.findIndex((subMenu) => subMenu.name === clothingContextMenuName);
-            if (index !== -1) {
-                item.customSubMenus.splice(index, 1);
-            }
-        }
+//         //FIXME: Workaround - prevent double creation by removing old menue with same name
+//         if (item.customSubMenus) {
+//             let index = item.customSubMenus.findIndex((subMenu) => subMenu.name === clothingContextMenuName);
+//             if (index !== -1) {
+//                 item.customSubMenus.splice(index, 1);
+//             }
+//         }
 
-        //Example for context menu
-        if (!item.customEventsToCall) {
-            item.customEventsToCall = [];
-        }
+//         //Example for context menu
+//         if (!item.customEventsToCall) {
+//             item.customEventsToCall = [];
+//         }
 
-        item.customEventsToCall.push({ name: contextMenuName, eventToCall: ExampleActions.Action1 });
+//         item.customEventsToCall.push({ name: contextMenuName, eventToCall: ExampleActions.Action1 });
 
-        //Example for submenues
+//         //Example for submenues
 
-        if ((item as StoredItem<ISharedItem>).data.type === 'clothing') {
-            if (!item.customSubMenus) {
-                item.customSubMenus = [];
-            }
+//         if ((item as StoredItem<ISharedItem>).data.type === 'clothing') {
+//             if (!item.customSubMenus) {
+//                 item.customSubMenus = [];
+//             }
 
-            item.customSubMenus.push({
-                name: clothingContextMenuName,
-                isOpen: false,
-                contextActions: [
-                    {
-                        name: 'action 1',
-                        eventToCall: ExampleActions.Action1,
-                    },
-                ],
-                customSubMenus: [
-                    {
-                        name: 'Sub menue 1',
-                        isOpen: false,
-                        contextActions: [
-                            {
-                                name: 'sub menu action 1',
-                                eventToCall: ExampleActions.Action1,
-                            },
-                            {
-                                name: 'sub menu action 2',
-                                eventToCall: ExampleActions.Action1,
-                            },
-                        ],
-                    },
-                ],
-            });
-        }
+//             item.customSubMenus.push({
+//                 name: clothingContextMenuName,
+//                 isOpen: false,
+//                 contextActions: [
+//                     {
+//                         name: 'action 1',
+//                         eventToCall: ExampleActions.Action1,
+//                     },
+//                 ],
+//                 customSubMenus: [
+//                     {
+//                         name: 'Sub menue 1',
+//                         isOpen: false,
+//                         contextActions: [
+//                             {
+//                                 name: 'sub menu action 1',
+//                                 eventToCall: ExampleActions.Action1,
+//                             },
+//                             {
+//                                 name: 'sub menu action 2',
+//                                 eventToCall: ExampleActions.Action1,
+//                             },
+//                         ],
+//                     },
+//                 ],
+//             });
+//         }
 
-        return item;
-    }
-}
+//         return item;
+//     }
+// }
--- src/core/plugins/gp-athena-overrides/server/examples/index.ts
+++ src/core/plugins/gp-athena-overrides/server/examples/index.ts
@@ -1,11 +1,11 @@
 import * as Athena from '@AthenaServer/api/index.js';
 import { Config } from '@AthenaPlugins/gp-athena-overrides/shared/config.js';
-import { InventoryContextMenuExample } from './InventoryContextMenuExample.js';
+// import { InventoryContextMenuExample } from './InventoryContextMenuExample.js';
 
 export class Examples {
     static init() {
         if (!Config.EXAMPLES_ENABLED) return;
 
-        InventoryContextMenuExample.init();
+        // InventoryContextMenuExample.init();
     }
 }
